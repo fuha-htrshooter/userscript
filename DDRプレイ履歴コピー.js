6c073e1496cda6d448ab7b9727594b7ade88baaa
fuha <44752205+fuha-htrshooter@users.noreply.github.com>
2026-05-30 04:48:48 +0900

    Update DDRプレイ履歴コピー.js サイト構造変化に対応
    ・送信時タイムアウトを追加
    ・送信しないを選んだ場合、ダイアログで送信予定だった内容を表示するよう変更

--- DDRプレイ履歴コピー.js
+++ DDRプレイ履歴コピー.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DDRプレイ履歴送信
 // @namespace    https://github.com/fuha-htrshooter/
-// @version      0.5
+// @version      0.6
 // @description  「最近プレーした楽曲」画面に、表内容をJSONでPOSTするボタンを追加する
 // @match        https://p.eagate.573.jp/game/ddr/ddr*/playdata/music_recent.html
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=p.eagate.573.jp
@@ -19,68 +19,67 @@ function add_button(appendParent, addEventFunction) {
 (function() {
     'use strict';
 
-    // 送信先URL
+    // 受信したJSONを元に差分登録するスクリプト
     const GAS_URL = "https://script.google.com/macros/s/AKfycbxrmfvUThg6E4_-X--TlWVoB1wleEPL5mDWgKngTDlbPdJ7cmhuM2N8myr4wCOGlzxb/exec";
 
     add_button(document.getElementsByTagName("h2")[0], function (e) {
-        // 送信確認
-        if (!confirm("送信します。よろしいですか？")) {
-            return;
-        }
-
         // 出力バッファ
         var data = [];
 
         // 行ごとにテーブル処理
-        $('#data_tbl tr').each(function(rownum) {
-            // 1行分のバッファ配列
-            var row = [];
-
-            // 列ごとに行処理
-            $(this).find('td').each(function(colnum) {
-                if (colnum == 1) {
-                    // 難易度
-                    var styleTxt = $(this).find('[class="style"]').text().replace("DOUBLE","DP-").replace("SINGLE","SP-");
-                    var difficultTxt = $(this).find('[class="difficulty"]').text();
-                    row.push(styleTxt + difficultTxt);
-                } else if (colnum == 2) {
-                    // ランク
-                    var rankSrc = $(this).find('img[src^="/game/ddr/ddrworld/images/playdata/rank_"]').attr('src');
-                    var rankTxt = rankSrc.split('/').pop().split('?')[0].replace(/rank_s_|\.png/g, "");
-                    row.push(rankTxt.replace("_p", "+").replace("_m", "-").toUpperCase());
-                } else {
-                    // 上記以外
-                    row.push($(this).text().trim());
-                }
-            });
+        $('table.table-ui tbody tr').each(function(rownum) {
+            // 曲名
+            const name = $(this).find('.music-name').text().trim();
+            // 難易度
+            const difficulty = $(this).find('.diff-style-container .diff').contents()
+            .filter(function(){return this.nodeType==Node.TEXT_NODE}).text().trim();
+            const style = $(this).find('.diff-style-container .level').text().trim()
+            .replace("DOUBLE","DP-").replace("SINGLE","SP-");
+            // スコア
+            const score = $(this).find('.score').text().trim();
+            // ランク
+            const rankSrc = $(this).find('img[src^="/game/ddr/ddrworld/images/playdata/rank_"]').attr('src');
+            const rank = rankSrc.split('/').pop().split('?')[0].replace(/rank_s_|\.png/g, "").replace("_p", "+").replace("_m", "-").toUpperCase();
+            // 日時
+            const timestamp =  $(this).find('.date').text().trim();
 
             // 1行分を追加
             data.push({
-                name: row[0],
-                chart: row[1],
-                rank: row[2],
-                score: row[3],
-                timestamp: row[4]
+                name: name,
+                chart: style + difficulty,
+                rank: rank,
+                score: score,
+                timestamp: timestamp
             });
         });
 
-        // 送信実行
-        GM_xmlhttpRequest({
-            method: "POST",
-            url: GAS_URL,
-            data: JSON.stringify(data),
-            headers: {
-                "Content-Type": "application/json"
-            },
-            onload: res => {
-                alert(res.responseText);
-            },
-            onerror: res => {
-                alert("[error] status: " + res.status + "\n" + res.responseText);
-            },
-            onabort: res => {
-                alert("[abort] status: " + res.status + "\n" + res.responseText);
-            },
-        });
+        // 送信確認
+        if (confirm("送信しますか？\nいいえの場合ダイアログ表示のみ行って終了します。")) {
+            // 送信実行
+            GM_xmlhttpRequest({
+                method: "POST",
+                url: GAS_URL,
+                timeout: 10000,
+                data: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                onload: res => {
+                    alert(res.responseText);
+                },
+                onerror: res => {
+                    alert("[error] status: " + res.status + "\n" + res.responseText);
+                },
+                onabort: res => {
+                    alert("[abort] status: " + res.status + "\n" + res.responseText);
+                },
+                ontimeout: res => {
+                    alert("[timeout] status: " + res.status + "\n" + res.responseText);
+                }
+            });
+        } else {
+            // ダイアログ表示
+            alert(JSON.stringify(data));
+        }
     });
 })();
